05a51cfcca331fb0e7dc2f09dae8ac3b901609b5
Mateusz Antos <219296@student.pwr.edu.pl>
2015-05-28 00:21:56 +0200

    dno2

--- zero.cpp
+++ zero.cpp
@@ -6,11 +6,7 @@ bool Zero::isZero()
 	return true; 
 }
 
-<<<<<<< HEAD
 Zero::Zero(string text, Player& player):Event(text, player)
-=======
-Zero::Zero()
->>>>>>> 4ea4e705dc97fb1270f89fdcf49c10188fa89fd7
 {
 	text="Nic tu nie ma\n";
 }
