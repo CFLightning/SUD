349ee3cd461968743933793f682678e5f2bf1f7f
Mateusz Antos <219296@student.pwr.edu.pl>
2015-05-28 00:55:46 +0200

    interactions

--- interaction.cpp
+++ interaction.cpp
@@ -1,1 +1,17 @@
+#include "interaction.h"
 
+bool Interaction::isInteraction()
+{
+	return true;
+}
+
+Interaction::Interaction(string text, Player& player, 
+	int bonusHP, int bonusDMG, int DMGamount, int lvlUp, int gold): Event(text, player)
+{
+	this->text=text;
+	this->bonusHP=bonusHP;
+	this->bonusDMG=bonusDMG;
+	this->DMGamount=DMGamount;
+	this->lvlUp=lvlUp;
+	this->gold=gold;
+}
